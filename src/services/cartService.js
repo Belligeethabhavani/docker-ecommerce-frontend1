c4d2b641494ee149b7153a73cea43e1bcb642723
Belli Geetha Bhavani <163846705+Belligeethabhavani@users.noreply.github.com>
2025-09-18 11:45:39 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-54-242-252-177.compute-1.amazonaws.com:8081/api/cart";
+const API_URL = "http://ec2-107-21-21-50.compute-1.amazonaws.com:8081/api/cart";
 
-const API_BASE = "http://ec2-54-242-252-177.compute-1.amazonaws.com:8081/api/cart";
+const API_BASE = "http://ec2-107-21-21-50.compute-1.amazonaws.com:8081/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${API_BASE}/add`, {
